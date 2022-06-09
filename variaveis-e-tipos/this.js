472fa4e189f2eb47476ed4281617dd62660a9f2c
Henderson Willian <hendersonled@gmail.com>
2022-06-09 20:44:03 -0300

    adiciona exercicios

--- variaveis-e-tipos/this.js
+++ variaveis-e-tipos/this.js
@@ -21,3 +21,9 @@ const pessoa3 = {
 
 console.log(calculaIdade.call(pessoa3, 40));
 console.log(calculaIdade.apply(pessoa2, [24]));
+
+// call - colocamos o objeto ao qual desejamos que o this se refira
+
+// Apply - Enquanto no call os argumentos sao enviados separados por virgula, no apply eles são enviados por meio de arrays
+
+// Bind - Clona a estrutura da função onde é chamada e aplica o valor do objeto passado como parâmetro

b1a3a82e9479093b4b3848501bcfa0c1a4a0d2a5
Henderson Willian <hendersonled@gmail.com>
2022-06-16 14:21:03 -0300

    Adiciona desafios

---
+++ Introducao-pratica-TypeScript/Respostas/desafio01.ts
@@ -0,0 +1,7 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+// declarando os parametros e seus tipos de forma implicita
+
+let employee = {
+    code: 10,
+    name: 'John'
+};
--- Introducao-pratica-TypeScript/desafio01.js
+++
@@ -1,5 +0,0 @@
-// Como podemos rodar isso em um arquivo .ts sem causar erros? 
-// Resposta: Declarando as variaveis usadas nos constructors e definindo um valor padrão para ela de acordo com o tipo que será atribuido posteriormente.
-let employee = {name: '', code: 0};
-employee.code = 10;
-employee.name = "John";

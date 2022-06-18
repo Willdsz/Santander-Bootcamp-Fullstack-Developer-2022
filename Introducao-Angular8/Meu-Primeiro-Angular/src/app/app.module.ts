4f2461a106851f47e3c7a08fb16c2cc3d98e646e
Henderson Willian <hendersonled@gmail.com>
2022-06-18 12:54:17 -0300

    Segregando a Aplicação de Módulos dos Cursos

--- Introducao-Angular8/Meu-Primeiro-Angular/src/app/app.module.ts
+++ Introducao-Angular8/Meu-Primeiro-Angular/src/app/app.module.ts
@@ -11,28 +11,20 @@ import { ReplacePipe } from './pipe/replace.pipe';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { Error404Component } from './error-404/error-404.component';
 import { CourseInfoComponent } from './coursers/course-info.component';
+import { CourseModule } from './coursers/course.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseListComponent,
-    StarComponent,
-    ReplacePipe,
     NavBarComponent,
     Error404Component,
-    CourseInfoComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpClientModule,
+    CourseModule,
     RouterModule.forRoot([
-      {
-        path: 'courses', component: CourseListComponent
-      },
-      {
-        path: 'courses/info/:id', component: CourseInfoComponent
-      },
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
       },
---
+++ Introducao-Angular8/Meu-Primeiro-Angular/src/app/coursers/course.module.ts
@@ -0,0 +1,33 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { ReplacePipe } from "../pipe/replace.pipe";
+import { StarComponent } from "../star/star.component";
+import { CourseInfoComponent } from "./course-info.component";
+import { CourseListComponent } from "./course-list.component";
+
+@NgModule({
+    declarations: [
+        CourseListComponent,
+        CourseInfoComponent,
+        ReplacePipe,
+        StarComponent
+    ], 
+    imports: [
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([
+            {
+                path: 'courses', component: CourseListComponent
+            },
+            {
+                path: 'courses/info/:id', component: CourseInfoComponent
+            }
+        ])
+    ]
+})
+
+export class CourseModule {
+
+}

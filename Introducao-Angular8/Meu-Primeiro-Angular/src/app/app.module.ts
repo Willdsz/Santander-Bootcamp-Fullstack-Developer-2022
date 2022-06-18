09f89d62a7277da6adaca03112539dd359c5e637
Henderson Willian <hendersonled@gmail.com>
2022-06-18 14:02:40 -0300

    Adiciona Tratamento de Erros ao modulo Core

--- Introducao-Angular8/Meu-Primeiro-Angular/src/app/app.module.ts
+++ Introducao-Angular8/Meu-Primeiro-Angular/src/app/app.module.ts
@@ -5,14 +5,12 @@ import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { Error404Component } from './error-404/error-404.component';
 import { CourseModule } from './coursers/course.module';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Error404Component,
   ],
   imports: [
     BrowserModule,
@@ -23,9 +21,6 @@ import { CoreModule } from './core/core.module';
     RouterModule.forRoot([
       {
         path: '', redirectTo: 'courses', pathMatch: 'full'
-      },
-      {
-        path: '**', component: Error404Component
       }
     ])
   ],

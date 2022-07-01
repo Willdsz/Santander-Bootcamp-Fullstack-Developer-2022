07abb612fb3518c5931e4f6500ce1650eec8d1b4
Henderson Willian <hendersonled@gmail.com>
2022-06-30 23:35:00 -0300

    Adicionando os conteúdos do CRUD

--- Introducao-a-Servicos-Assincronos-no-Angular/crud-dio-angular/src/app/app.module.ts
+++ Introducao-a-Servicos-Assincronos-no-Angular/crud-dio-angular/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { UsersListComponent } from './pages/users/users-list/users-list.component';
+import { UserFormComponent } from './pages/users/user-form/user-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
+    UsersListComponent,
+    UserFormComponent,
   ],
   imports: [
     BrowserModule,

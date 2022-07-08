bc76a2db38accd1a321d024fb6ca3702d4980134
willdsz <hendersonled@gmail.com>
2022-07-08 11:02:03 -0300

    Adiciona o meu primeiro programa java feito no notepad e um outro feito no eclipse IDE

---
+++ Dominando-IDES-JAVA/primeiro-programa-java/src/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+public class PrimeiroPrograma {
+	
+	public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println( "Hello World! " + (a + b) );
+	}
+
+}
+
+

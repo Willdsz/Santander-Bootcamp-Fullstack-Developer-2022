2b6f6cb9514334f21ef7a24e9b9e5b19c46bca6a
willdsz <hendersonled@gmail.com>
2022-07-11 10:19:08 -0300

    fdosfjsfja

---
+++ Dominando-IDES-JAVA/primeiro-programa-java/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,21 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+	
+	Gato gato = new Gato();
+	Livros livros = new Livros();
+	
+	/*public static void main(String[] args) {
+		int a = 2;
+		int b = 3;
+		System.out.println( "Hello World! " + (a + b) );
+	}*/
+
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}

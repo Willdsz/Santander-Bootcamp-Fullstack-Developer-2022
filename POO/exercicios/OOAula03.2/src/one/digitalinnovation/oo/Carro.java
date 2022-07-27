7d8c982fb9093cded330c6414e7ad4677ed12615
Henderson Willian <hendersonled@gmail.com>
2022-07-27 20:25:37 -0300

    Adiciona exercicio 3 de POO

--- POO/exercicios/OOAula03.2/src/one/digitalinnovation/oo/Carro.java
+++ POO/exercicios/OOAula03.2/src/one/digitalinnovation/oo/Carro.java
@@ -4,7 +4,7 @@ package one.digitalinnovation.oo;
  * Classe de exemplo para o exercício da Aula 2 de Orientação a Objetos.
  */
 class Carro {
-
+//Em java, é necessário declarar o tipo de todas as variaveis e atributos criados
     String cor;
     String modelo;
     int capacidadeTanque;

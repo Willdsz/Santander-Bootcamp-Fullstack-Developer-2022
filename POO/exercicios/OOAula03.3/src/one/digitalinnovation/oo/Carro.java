f0a7e7d28e00222d497a282c7343844f7fa68f8a
Henderson Willian <hendersonled@gmail.com>
2022-07-27 20:59:14 -0300

    Adiciona anotações

--- POO/exercicios/OOAula03.3/src/one/digitalinnovation/oo/Carro.java
+++ POO/exercicios/OOAula03.3/src/one/digitalinnovation/oo/Carro.java
@@ -3,16 +3,18 @@ package one.digitalinnovation.oo;
 /**
  * Classe de exemplo para o exercício da Aula 2 de Orientação a Objetos.
  */
+//Cria a class Carro
 class Carro {
-
+//    Define os Atributos da Classe
     String cor;
     String modelo;
     int capacidadeTanque;
 
+//    Declara o Construtor | Deixar os construtores logo abaixo dos atributos é uma boa prática
     Carro() {
 
     }
-
+//  Sobrecarga do Construtor logo abaixo
     Carro(String cor, String modelo, int capacidadeTanque) {
         this.cor = cor;
         this.modelo = modelo;
